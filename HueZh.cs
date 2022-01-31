b008d301b7f33ae9dd341d61bcf10db46f973613
Sheep-y <your.sheepy@gmail.com>
2022-01-31 21:34:11 +0800

    Refactor code, add update count, remove english column for size and legal, and fix subtitle. Subtitle works. Dialogue works. Need to test a bit more.

--- HueZh.cs
+++ HueZh.cs
@@ -26,9 +26,12 @@ namespace HueZh {
    internal class HuePatcher : Patcher {
       private static string TextPath = Path.Combine( Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location ), "HueZh.csv" );
 
-      internal void Apply () => Patch( typeof( LocalizedText ), "SetToLanguage", nameof( OverrideLanguage ) );
+      internal void Apply () {
+         Patch( typeof( LocalizedText ), "SetToLanguage", nameof( OverrideLocalizedText ) );
+         Patch( typeof( Subtitles ), "SetToLanguage", nameof( OverrideSubtitles ) );
+      }
 
-      private static string ReadData () {
+      private static string LoadData () { lock( TextPath ) {
          if ( File.Exists( TextPath ) ) {
             Info( "Loading data from {0} ({1} bytes).  The file is user editable.  Delete it to reset to default.", TextPath, new FileInfo( TextPath ).Length );
             try { return File.ReadAllText( TextPath ); } catch ( SystemException x ) { Warn( x ); }
@@ -41,56 +44,56 @@ namespace HueZh {
             Fine( "{1} bytes written to {0}", TextPath, new FileInfo( TextPath ).Length );
          } catch ( SystemException x ) { Warn( x ); }
          return data;
-      }
+      } }
 
-      private static void OverrideLanguage ( string[] lines, ref int languageID, ref string ___selectedLanguage, Dictionary< string, int > ___languages ) { try {
+      private static void OverrideLocalizedText ( string[] lines, ref int languageID, ref string ___selectedLanguage ) { try {
          if ( lines == null || lines.Length <= 1 ) return;
+         languageID = 1;
          if ( ___selectedLanguage == "chinese" ) { Info( "Game language is already chinese, index {0}.", languageID ); return; }
          Info( "Original game language is {0}.  {1} lines found.", ___selectedLanguage, lines.Length );
          #if DEBUG
-         File.WriteAllText( Path.Combine( RootMod.AppDataDir, "Orig.csv" ), string.Join( "\r\n", lines ) );
+         lock ( TextPath ) File.WriteAllText( Path.Combine( RootMod.AppDataDir, "Orig.csv" ), string.Join( "\r\n", lines ) );
          #endif
+         OverrideLanguage( lines );
+         ___selectedLanguage = "chinese";
+         lines[ 0 ] = "Column,chinese";
+      } catch ( Exception ex ) { Err( ex ); } }
 
+      private static void OverrideSubtitles ( ref int languageID ) => languageID = 1;
+
+      private static void OverrideLanguage ( string[] lines ) { try {
          var map = new Dictionary< string, string >();
          var blank = new HashSet< string >();
          var buffer = new StringBuilder();
-         var r = new StringReader( ReadData() );
+         var r = new StringReader( LoadData() );
          while ( r.TryReadCsvRow( out var line, buffer ) ) {
             var cells = line.ToArray();
-            if ( cells.Length < 3 || cells[ 0 ].Length == 0 || ( cells[ 0 ] == "Column" && cells[ 1 ] == "english" ) ) continue;
-            if ( cells[ 2 ].Length == 0 ) { blank.Add( cells[ 0 ] ); continue; }
+            if ( cells.Length < 2 || cells[ 0 ].Length == 0 || ( cells[ 0 ] == "Column" && cells[ 1 ] == "english" ) ) continue;
+            if ( cells[ 1 ].Length == 0 ) { blank.Add( cells[ 0 ] ); continue; }
             Fine( "Loaded {0}", cells[ 0 ] );
-            map[ cells[ 0 ] ] = cells[ 2 ].Replace( '\r', ' ' ).Replace( '\n', ' ' );
+            map[ cells[ 0 ] ] = cells[ 1 ].Replace( '\r', ' ' ).Replace( '\n', ' ' );
          }
          Info( "Chinese data loaded ({0} translated, {1} keep original).", map.Count, blank.Count );
          if ( map.Count <= 1 ) {
             Error( "Too few entries, something is wrong.  Aborting." );
             return;
          }
-
+         var count = 0;
          for ( var i = lines.Length - 1 ; i >= 1 ; i-- ) {
-            var cells = new StringReader( lines[ i ] ).ReadCsvRow( buffer ).ToArray();
-            if ( cells.Length <= 1 || cells[ 0 ].Length == 0 ) continue;
+            var cells = new StringReader( lines[ i ] ).ReadCsvRow( buffer )?.ToArray();
+            if ( cells == null || cells.Length <= 1 || cells[ 0 ].Length == 0 ) continue;
             var key = cells[ 0 ];
             if ( ! map.TryGetValue( key, out var text ) ) {
                cells = new StringReader( lines[ i ] ).ReadCsvRow().ToArray();
                if ( ! blank.Contains( key ) ) Info( "Untranslated: {0} => {1}", key, cells[ 1 ] );
                text = cells[ 1 ].Length == 0 ? "?" : cells[ 1 ];
-            }
+            } else
+               count++;
             var line = new StringBuilder().AppendCsvLine( key, text ).ToString();
             Fine( "Updating {0}", key );
             lines[ i ] = line;
          }
-
-         Info( "Chinese data injected.  Changing game language to chinese." );
-         languageID = 1;
-         ___selectedLanguage = "chinese";
-         lines[0] = "Column,english";
-         if ( ___languages.TryGetValue( "english", out var pos ) && pos == 1 ) {
-            ___languages.Clear();
-            ___languages.Add( "chinese", 1 );
-         } else
-            Warn( "Unexpected game language list; english not at col 1: {0}", string.Join( ", ", ___languages.OrderBy( e => e.Value ).Select( e => $"{e.Value}:{e.Key}" ).ToArray() ) );
+         Info( "{0} entries updated.", count );
       } catch ( Exception ex ) { Err( ex ); } }
    }
 

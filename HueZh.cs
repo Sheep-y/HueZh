4733eca44740d33924a2048fbd0da76b9f026ab9
Sheep-y <your.sheepy@gmail.com>
2022-01-31 13:43:44 +0800

    Embed text data into dll, will create text file which is still preferred.

--- HueZh.cs
+++ HueZh.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -27,13 +28,28 @@ namespace HueZh {
 
       internal void Apply () => Patch( typeof( LocalizedText ), "SetToLanguage", nameof( OverrideLanguage ) );
 
+      private static string ReadData () {
+         if ( File.Exists( TextPath ) ) {
+            Info( "Loading data from {0} ({1} bytes).  The file is user editable.  Delete it to reset to default.", TextPath, new FileInfo( TextPath ).Length );
+            try { return File.ReadAllText( TextPath ); } catch ( SystemException x ) { Warn( x ); }
+         }
+         Info( "Loading from build-in data ({0} bytes compressed).", Resource.HueZh_csv_gz.Length );
+         var data = new StreamReader( new DeflateStream( new MemoryStream( Resource.HueZh_csv_gz, false ), CompressionMode.Decompress ), Encoding.UTF8 ).ReadToEnd();
+         Info( "Recreating {0}", TextPath );
+         try {
+            File.WriteAllText( TextPath, data, Encoding.UTF8 );
+            Fine( "{1} bytes written to {0}", TextPath, new FileInfo( TextPath ).Length );
+         } catch ( SystemException x ) { Warn( x ); }
+         return data;
+      }
+
       private static void OverrideLanguage ( string[] lines, ref int languageID, ref string ___selectedLanguage, Dictionary< string, int > ___languages  ) { try {
          if ( lines == null ) return;
          Info( "Original game language is {0}.  {1} lines found.", ___selectedLanguage, lines.Length );
 
          var comma = new char[]{ ',' };
          var map = new Dictionary< string, string >();
-         foreach ( var line in File.ReadAllText( TextPath ).Split( new string[]{ "\r\n" }, StringSplitOptions.None ) ) {
+         foreach ( var line in ReadData().Split( new string[]{ "\r\n" }, StringSplitOptions.None ) ) {
             if ( line.Length == 0 ) continue;
             var cells = line.Split( comma, 2, StringSplitOptions.None );
             if ( cells.Length <= 1 || cells[ 0 ].Length == 0 ) continue;
@@ -66,11 +82,7 @@ namespace HueZh {
          } else
             Warn( "Unexpected game language list: {0}", string.Join( ", ", ___languages.OrderBy( e => e.Value ).Select( e => $"{e.Value}:{e.Key}" ).ToArray() ) );
 
-      } catch ( FileNotFoundException ex ) {
-         Error( "Not Found: {0}", TextPath );
-      } catch ( Exception ex ) {
-         Err( ex );
-      } }
+      } catch ( Exception ex ) { Err( ex ); } }
    }
 
 }
